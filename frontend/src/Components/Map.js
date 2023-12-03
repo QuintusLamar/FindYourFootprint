bc8b560422f56fa88684f61aca65621fe09bf979
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2023-12-02 19:29:23 -0500

    added endpoint markers

--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, Polyline, Marker } from 'react-leaflet';
+import marker from "./Vector4.svg";
+import { Icon } from 'leaflet'
 
 function Map({ routePoints }) {
-  // const [testRoute, setTestRoute] = useState(<></>);
-  // const [isTest, setisTest] = useState(false);
-
-
-  // let testRoute = <Polyline />;
   let testRoute;
+  let startMarker;
+  let endMarker;
+
   if (routePoints) {
     const flippedPoints = routePoints.map(p => [p[1], p[0]]);
-    testRoute = (<Polyline
-      positions={flippedPoints}
-    />)
-    // setisTest(true);
+    testRoute = (
+      <Polyline
+        positions={flippedPoints}
+      />
+    );
+    const myIcon = new Icon({
+      iconUrl: marker,
+      iconSize: [32,32]
+     })
+    startMarker = <Marker position={flippedPoints[0]}  icon={myIcon}/>;
+    endMarker = <Marker position={flippedPoints[flippedPoints.length - 1]}  icon={myIcon} />;
   }
 
   return (
     <MapContainer
-      // center={[37.7749, -122.4194]}  // Cali testing
-      center={[33.775, -84.393]}   // Atlanta coord
+      center={[33.775, -84.393]}
       zoom={16}
       style={{ height: '100%', width: '100%' }}
-      zoomControl={false} // Set zoomControl to false to hide zoom buttons
+      zoomControl={false}
     >
+      {startMarker && startMarker}
+      {endMarker && endMarker}
       <TileLayer
         url="https://api.mapbox.com/styles/v1/quintus-lamar/cloui2cuk00jx01qofx3w1x1k/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicXVpbnR1cy1sYW1hciIsImEiOiJjbG91ZDJwYzMwaHc5MmtvOW82cm9uZGhnIn0.VL3TfnwBOoiek2f8bxw4RQ"
         attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
@@ -34,6 +42,7 @@ function Map({ routePoints }) {
       {testRoute && testRoute}
     </MapContainer>
   );
-};
+}
+
 
 export default Map;
